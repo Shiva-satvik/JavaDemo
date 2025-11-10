7710596f9cdc6449cc7ba955275c7b367c996e61
Shiva-satvik <satvikjakkam@gmail.com>
2025-11-10 14:01:11 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-System.out.println("Hello, Jenkins with Git!");
+        
+System.out.println("Hello, Jenkins witht!");
        
     }
 }
